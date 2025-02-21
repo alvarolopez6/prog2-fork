cf28be32bfe3cbc3eefff5bec3c09e36d968fc33
Younes Laihi <yl74@alu.ua.es>
2025-02-21 09:53:35 +0100

    Update estudiante.py

--- estudiante.py
+++ estudiante.py
@@ -1,8 +1,23 @@
 class Estudiante:
     total_estudiantes = 0  # Contador de instancias
 
-    def __init__(self, nombre, edad, cursos_inscritos):
+    def __init__(self, nombre:str, edad:int, cursos_inscritos:list[str]):
         self.nombre = nombre
         self.edad = edad
         self.cursos_inscritos = cursos_inscritos
         Estudiante.total_estudiantes += 1  # Aumenta el contador cada vez que se crea un estudiante
+    def mostrar_informacion(self):
+        print(repr(self))
+    def __str__(self):
+        return f"{self.nombre},{self.edad},{self.cursos_inscritos}"
+    def __repr__(self):
+        return f"nombre:{self.nombre}, edad:{self.edad}, cursos:{self.cursos_inscritos}"
+    @staticmethod
+    def es_mayor_de_edad(edad):
+        if edad>=18:
+            return True
+        else:
+            return False
+    @classmethod
+    def desde_tupla(cls,tupla):
+        return cls(*tupla)
